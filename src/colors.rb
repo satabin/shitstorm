c80a9c9cde40cd06b60388df0556bee5babdc18f
Lucas Satabin <lucas.satabin@gnieh.org>
2018-10-07 13:36:59 +0200

    Changement de couleur des twittos
    Un truc un peu moins moche j’espère...

--- src/colors.rb
+++ src/colors.rb
@@ -1,7 +1,7 @@
 def color_by_type(tpe)
   case tpe
   when "Anti-Vax" then '#4B698B'
-  when "Twitto" then '#CA6573'
+  when "Twitto" then '#AA3939'
   when "Platiste" then '#D4AB6A'
   when "9/11" then '#70B45A'
   else '#764B8E'
